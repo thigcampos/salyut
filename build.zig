e3d93797857129d4ccec1c1e7055d3dfcc5be0e2
ivansantiagojr <ivansantiago.junior@gmail.com>
2025-06-29 15:40:50 -0300

    Set release options (with strip)

--- build.zig
+++ build.zig
@@ -2,12 +2,13 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSafe });
 
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+        .strip = true,
     });
 
     const exe = b.addExecutable(.{
