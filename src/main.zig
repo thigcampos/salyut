686cdf9995d5c95a5245e6b33934c95cbe59593a
ivansantiagojr <ivansantiago.junior@gmail.com>
2025-07-13 18:36:02 -0300

    WIP: represent initial toml proposal in Zig structs

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,40 @@
-pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
+const toml = @import("toml");
 
+const DotfilesHashMap = struct {
+    source: []const u8,
+    destination: []const u8,
+};
+const Dotfiles = struct {
+    files: []DotfilesHashMap,
+};
+
+const Dot = struct {
+    dotpath: []const u8,
+};
+
+// Represents the whole dot.toml config file
+const Config = struct {
+    dot: Dot,
+    dotfiles: Dotfiles,
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var parser = toml.Parser(Config).init(allocator);
+    defer parser.deinit();
+
+    var result = try parser.parseFile("./examples/example.toml");
+    defer result.deinit();
+
+    const config = result.value;
+
+    std.debug.print("{s}\n", .{config.dot.dotpath});
+
+    for (config.dotfiles.files) |file| {
+        std.debug.print("{s}\n", .{file.source});
+    }
+}
